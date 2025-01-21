e8f133db436131e191af5b29add0402b988a1109
Joan <joanmarchernandezmateu@a.cifpperedesongall.org>
2025-01-21 18:49:27 +0100

    Segundo commit

--- inventario_instituto/src/Main.java
+++ inventario_instituto/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    static void mostrarListaProd(String[] inventario) {
+        for (int i = 0; i < inventario.length; i++) {
+            System.out.println(inventario[i]);
+        }
+    }
     public static void main(String[] args) {
-        String[] inventari = {"Ordinador", "Taula", "Cadira", "Llapis", "Goma"};
+        String[] inventario = {"Ordinador", "Taula", "Cadira", "Llapis", "Goma"};
+        mostrarListaProd(inventario);
+        
     }
 }
