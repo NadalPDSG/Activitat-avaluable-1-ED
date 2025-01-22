a168a8a3c7633a0f7656a4af46b60c255505099f
Miguel-18-waza <miguelangelcardenasheredia@a.cifpperedesongall.org>
2025-01-22 16:02:48 +0100

    Eliminar producto por nombre

--- inventario_instituto/src/Main.java
+++ inventario_instituto/src/Main.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     static void mostrarListaProd(String[] inventario) {
         for (int i = 0; i < inventario.length; i++) {
@@ -7,6 +11,19 @@ public class Main {
     public static void main(String[] args) {
         String[] inventario = {"Ordinador", "Taula", "Cadira", "Llapis", "Goma"};
         mostrarListaProd(inventario);
-        
+        ElimProd(inventario);
+        mostrarListaProd(inventario);
+
+    }
+    public static void ElimProd(String[] inventario) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el elemento que quieres eliminar: ");
+        String EleEliminar = sc.nextLine();
+        for (int i = 0; i < inventario.length; i++) {
+            if (EleEliminar.equals(inventario[i])){
+                inventario[i] = "";
+            }
+        }
+
     }
 }
